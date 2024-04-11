48a55d34d447de0d34a09b2e457b4040b0b17fc3
JaeHyeon Kim <tlwkr96@naver.com>
2024-04-12 02:32:40 +0900

    project06_first

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,20 @@
+import "./ContactList.css";
+import ContactItem from "./ContactItem";
+import { useState } from "react";
+
+export default function ContactList({ contacts, onDelete }) {
+  const getFilterdData = () => {
+    return contacts.filter((contact) => contact.content);
+}
+
+  const filteredContacts = getFilterdData();
+  
+  return (
+    <div className="ContactList">
+      <div className="title">Contact List</div>
+      {filteredContacts.map((contact) => {
+        return <ContactItem className="ContactItem" key={contacts.id} {...contact} onDelete={onDelete} />;
+      })}
+    </div>
+  );
+}
